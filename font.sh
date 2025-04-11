763a53528b9a9ef033b77f1ffa602004c57b86ef
hak53q <hak53q@gmail.com>
2025-04-12 00:40:00 +0800

    Update font.sh

--- font.sh
+++ font.sh
@@ -4,15 +4,7 @@ cd ~/dotfiles/fonts/
 git clone https://github.com/hak53q/Plangothic/
 cd ~/dotfiles/fonts/Plangothic/otf/
 makepkg -si
-cd ~/dotfiles/fonts/ms/Arial/
-makepkg -si
-cd ~/dotfiles/fonts/ms/PMingLiU/
-makepkg -si
-cd ~/dotfiles/fonts/ms/KaiU/
-makepkg -si
-cd ~/dotfiles/fonts/Kosugi/
-makepkg -si
-cd
-paru -S --needed noto-fonts-cjk ttf-fira-code otf-misans otf-misans-tc otf-misans-l3 otf-misans-latin
+cd ~
+paru -S --needed noto-fonts-cjk ttf-fira-code ttf-sarasa_ui-{tc,sc,hk,jp}
 mkdir -p ~/.config/fontconfig/
 cp ~/dotfiles/fonts/fonts.conf ~/.config/fontconfig/fonts.conf
