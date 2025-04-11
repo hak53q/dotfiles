0a1f37afd07d53b2babd2470ab03240d2b58ba45
hak53q <hak53q@gmail.com>
2025-04-12 02:25:20 +0800

    Update font.sh

--- font.sh
+++ font.sh
@@ -1,10 +1,8 @@
 sudo pacman -Syu --needed paru 
-git clone https://github.com/hak53q/dotfiles/
-cd ~/dotfiles/fonts/
 git clone https://github.com/hak53q/Plangothic/
-cd ~/dotfiles/fonts/Plangothic/otf/
+cd ~/Plangothic/otf/
 makepkg -si
 cd ~
-paru -S --needed noto-fonts-cjk ttf-fira-code ttf-sarasa_ui-{tc,sc,hk,jp}
+paru -S --needed otf-rubik ttf-fira-code ttf-sarasa_ui-{tc,sc,hc,jp}
 mkdir -p ~/.config/fontconfig/
 cp ~/dotfiles/fonts/fonts.conf ~/.config/fontconfig/fonts.conf
